8aed2f5cf9247f41d40e31618d482d11e5577373
gauravdvp <66694211+gauravdvp@users.noreply.github.com>
2021-01-19 11:32:43 +0530

    Dev branch changes.

--- src/main/java/junit_mockito_test/Demo.java
+++ src/main/java/junit_mockito_test/Demo.java
@@ -2,6 +2,6 @@ package junit_mockito_test;
 
 public class Demo {
     {
-        System.out.println("just to check git push from intelliJ.");
+        System.out.println("just to check git push from intelliJ from dev branch.");
     }
 }
